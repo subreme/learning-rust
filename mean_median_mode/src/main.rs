02dce21daf430ea2dc1c6eabb6b85f8c23fb5d06
Subreme <71085002+subreme@users.noreply.github.com>
2021-05-11 14:56:26 +0200

    Create "mean_median_mode"

---
+++ mean_median_mode/src/main.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+use std::io;
+
+fn main() {
+    println!("Mean Median Mode...");
+    let nums: Vec<f64> = prompt();
+    println!("\nMean: {}", mean(&nums));
+    println!("Median: {}", median(&nums));
+    println!("Mode: {}", {
+        let mut x = String::new();
+        for num in mode(&nums) {
+            x += &num.to_string();
+            x += " ";
+        }
+        x
+    });
+}
+
+fn prompt() -> Vec<f64> {
+    return loop {
+        println!("\nEnter a list of numbers:");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input!");
+        let mut nums: Vec<f64> = vec![];
+        for num in input.trim().split_whitespace() {
+            match num.parse::<f64>() {
+                Ok(x) => nums.push(x),
+                Err(_) => println!("Error: `{}` is not an integer!", num),
+            }
+        }
+        nums.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        break nums;
+    };
+}
+
+fn mean(nums: &[f64]) -> f64 {
+    if nums.len() == 1 {
+        nums[0]
+    } else {
+        nums.iter().sum::<f64>() / nums.len() as f64
+    }
+}
+
+fn median(nums: &[f64]) -> f64 {
+    if nums.len() == 1 {
+        nums[0]
+    } else if nums.len() % 2 == 1 {
+        nums[nums.len() / 2]
+    } else {
+        (nums[nums.len() / 2 - 1] + nums[nums.len() / 2]) / 2f64
+    }
+}
+
+fn mode(nums: &[f64]) -> Vec<f64> {
+    let mut freq = HashMap::new();
+    for num in nums {
+        *freq.entry(num.to_string()).or_insert(0) += 1;
+    }
+    let mut value = vec![];
+    let mut count: u32 = 0;
+    for (x, y) in freq {
+        if y >= count {
+            let num: f64 = match x.parse::<f64>() {
+                Ok(x) => x,
+                Err(_) => 0f64,
+            };
+            if y > count {
+                value = vec![];
+                value.push(num);
+                count = y;
+            } else {
+                value.push(num);
+            }
+        }
+    }
+    value
+}
